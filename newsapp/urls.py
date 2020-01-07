d28701cd4b58004f9795043b76182ee41f5e53a3
rakesh <rakesh.linux@gmail.com>
2020-01-07 23:31:44 +0530

    simple news website

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
-urlpatterns = [   
+from .views import index, home
+urlpatterns = [
     #path('', index(country_name='in'), name="index"),
+    path('', home, name="home"),
     path('<country_name>', index, name="index"),
 ]
--- newsapp/views.py
+++ newsapp/views.py
@@ -5,11 +5,47 @@ import json
 # Create your views here.
 
 
-def index(request,country_name):
-    if country_name=='':
-        country_name='in'
-        
-    url = ('https://newsapi.org/v2/top-headlines?country='+ country_name +'&pagesize=24&apiKey=2121032f088e4370affe13ca317f8d1a')
+def home(request):
+    country_name = 'in'
+    url = ('https://newsapi.org/v2/top-headlines?country=in&pagesize=24&apiKey=2121032f088e4370affe13ca317f8d1a')
+    response = requests.get(url)
+    data = json.loads(response.text)
+
+    name = []
+    author = []
+    title = []
+    desc = []
+    urls = []
+    img = []
+    pubat = []
+    content = []
+    for article in data['articles']:
+        name.append(article['source']['name'])
+        author.append(article['author'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        urls.append(article['url'])
+        img.append(article['urlToImage'])
+        pubat.append(article['publishedAt'])
+        content.append(article['content'])
+        # print(article['source']['name'])
+        # print(article['author'])
+        # print(article['title'])
+        # print(article['description'])
+        # print(article['url'])
+        # print(article['urlToImage'])
+        # print(article['publishedAt'])
+        # print(article['content'])
+        post = zip(name, author, title, desc, urls, img, pubat, content)
+    return render(request, 'newsapp/index.html', {'data': post, 'country': country_name})
+
+
+def index(request, country_name):
+    if country_name == '':
+        country_name = 'in'
+
+    url = ('https://newsapi.org/v2/top-headlines?country=in&category=' +
+           country_name + '&pagesize=24&apiKey=2121032f088e4370affe13ca317f8d1a')
     response = requests.get(url)
     data = json.loads(response.text)
     name = []
@@ -38,4 +74,4 @@ def index(request,country_name):
         # print(article['publishedAt'])
         # print(article['content'])
         post = zip(name, author, title, desc, urls, img, pubat, content)
-    return render(request, 'newsapp/index.html', {'data': post,'country':country_name})
+    return render(request, 'newsapp/index.html', {'data': post, 'country': country_name})
