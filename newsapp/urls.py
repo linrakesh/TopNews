d2b30802cf8247dfd5f9fb75ca11a90d4fac9927
rakesh kumar <rakesh@binarynote.com>
2020-01-08 14:17:11 +0530

    new changes

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import index
 urlpatterns = [   
     #path('', index(country_name='in'), name="index"),
-    path('<country_name>', index, name="index"),
+    path('/<country_name>', index, name="index"),
 ]
